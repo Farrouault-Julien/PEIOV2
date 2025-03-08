32e65c47d0797a5dbcfd99584ea40032909fe530
Farrouault-Julien <julien.farrouault@gmail.com>
2025-03-08 19:24:37 +0100

    motion div enlevée

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,8 +1,7 @@
-import { motion } from "framer-motion";
+
 export default function Home() {
   return (
     <div>
-      <motion.div ></motion.div>
         <h1 className="">🚀 Bienvenue sur mon Portfolio</h1>
     </div>
   );
